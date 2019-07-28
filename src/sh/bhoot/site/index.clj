1fd9999bd7a9390c2ffe7b9bb1bbdb2883aa2d79
Jayesh Bhoot <jayesh@bhoot.sh>
2019-07-28 15:40:40 +0530

    feat: add functional content and styling

--- src/sh/bhoot/site/index.clj
+++ src/sh/bhoot/site/index.clj
@@ -11,20 +11,30 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
       (include-css "/styles.css")]
     [:body
-      [:img {:src "https://www.gravatar.com/avatar/0c189999e2c7abcf648d662080912928?s=200"}]
-      [:ul
-        [:li [:a {:href "/blog"} "Blog"]]
-        [:li [:a {:href "/blog/tags"} "Tags"]]]
-      [:ul
-          (for [post posts]
-            [:li
-              [:a {:href (:permalink post)}(:title post)]])]
-      [:address
-        [:h2 "Find me on"]
-        [:ul
-          [:li [:a {:href "mailto:jayesh@bhoot.sh"} "jayesh@bhoot.sh"]]
-          [:li [:a {:href "https://github.com/jayesh-bhoot"} "GitHub"]]
-          [:li [:a {:href "https://stackoverflow.com/users/663911/jayesh-bhoot"} "Stack Overflow"]]
-          [:li [:a {:href "https://twitter.com/jayeshbhoot"} "Twitter"]]
-          [:li [:a {:href "https://medium.com/@jayesh.bhoot"} "Medium"]]
-          [:li [:a {:href "https://www.linkedin.com/in/jayesh-bhoot/"} "LinkedIn"]]]]]))
+      [:header
+        [:nav
+          [:ul {:class "nav-menu"}
+            [:li [:a {:href "/blog"} "blog/dates"]]
+            [:li [:a {:href "/blog/tags"} "blog/tags"]]]]]
+      [:main
+        [:article
+          [:h3 "what I think I look like"]
+          [:img {:src "https://www.gravatar.com/avatar/0c189999e2c7abcf648d662080912928?s=200"}]]
+        [:article 
+          [:h3 "thoughts in words"]
+          [:ul
+            (for [post posts]
+              [:li
+                [:a {:href (:permalink post)}(:title post)]])]]
+        [:article 
+          [:address
+            [:h3 "find me on"]
+            [:ul
+              [:li [:a {:href "mailto:jayesh@bhoot.sh"} "jayesh@bhoot.sh"]]
+              [:li [:a {:href "https://github.com/jayesh-bhoot"} "github"]]
+              [:li [:a {:href "https://stackoverflow.com/users/663911/jayesh-bhoot"} "stack overflow"]]
+              [:li [:a {:href "https://twitter.com/jayeshbhoot"} "twitter"]]
+              [:li [:a {:href "https://medium.com/@jayesh.bhoot"} "medium"]]
+              [:li [:a {:href "https://www.linkedin.com/in/jayesh-bhoot/"} "linkedin"]]]]]]
+      [:footer
+        [:span "Powered by perun and hiccup"]]]))
