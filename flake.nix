e30f18ece83b659360947543d6b83f3df411817a
Jayesh Bhoot <jayesh@bhoot.sh>
2022-02-08 00:05:46 +0530

    Convert into a pure HTML blog.
    Remove the org files and artifacts
    Also, switch to a per-topic folder, instead of separate posts, images,
    css folders

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" ];
+      createDevShell = system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          buildInputs = [
+            pkgs.soupault
+          ];
+        };
+    in
+    {
+      devShell = nixpkgs.lib.genAttrs systems createDevShell;
+    };
+}
