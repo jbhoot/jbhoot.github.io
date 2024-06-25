87c9efe595fd084028aab77f96952d63f4555754
Jayesh Bhoot <jysh@jysh.net>
2024-06-25 19:05:03 +0530

    use a more general class name to add `role=list` to lists which have been styled to not display as a list

--- site/index.js
+++ site/index.js
@@ -37,7 +37,7 @@ function addListRoleToTimelines() {
     // To fix Safari's decision to remove list role from
     // those lists which have `list-style-type: none` set in CSS:
     // https://www.scottohara.me/blog/2019/01/12/lists-and-safari.html
-    const nodes = document.querySelectorAll('.timeline');
+    const nodes = document.querySelectorAll('.list-style-type\\:none');
     for (const node of nodes) {
         node.setAttribute('role', 'list');
     }
