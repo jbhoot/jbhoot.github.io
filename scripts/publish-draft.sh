08cfb14d6f702b870c2890d5fd117f8d0ba1c746
Jayesh Bhoot <jayesh@bhoot.dev>
2024-09-13 21:47:36 +0530

    publish date with ordinal suffix

--- scripts/publish-draft.sh
+++ scripts/publish-draft.sh
@@ -11,7 +11,7 @@ site_dir=$2
 
 published_year=$(date +%Y)
 published_stamp=$(date '+%Y-%m-%d')
-published_displayed=$(date '+%d %b %Y')
+published_displayed=$(date '+%dXX %B %Y' | sed -e 's/11XX/11th/' -e 's/12XX/12th/' -e 's/13XX/13th/' -e 's/1XX/1st/' -e 's/2XX/2nd/' -e 's/3XX/3rd/' -e 's/XX/th/')
 published_ele=$(htmlq -f "$target_file" ".dt-published")
 
 sed -i.bkp -E "s|$published_ele|<time class=\"dt-published\" datetime=\"$published_stamp\">$published_displayed</time>|" "$target_file"
