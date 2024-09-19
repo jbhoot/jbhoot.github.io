c71dc15e34da38ebc4c417fdf148db686484dbea
Jayesh Bhoot <jayesh@bhoot.dev>
2024-09-20 00:58:12 +0530

    start using flake again

--- flake.nix
+++ flake.nix
@@ -2,28 +2,24 @@
   description = "Flake to manage the artifacts related to my personal website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=1daf1298306546c39b3ed0e336a63a02339d4d0a";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
   };
 
-  outputs = {self, ...} @ inputs: let
-    systems = ["x86_64-darwin" "aarch64-darwin" "x86_64-linux"];
-    createDevShell = system: let
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-      };
+  outputs =
+    { self, ... }@inputs:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+      createDevShell =
+        system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+        in
+        pkgs.mkShell { buildInputs = [ pkgs.soupault ]; };
     in
-      pkgs.mkShell {
-        buildInputs =
-          [
-            pkgs.soupault
-            # pkgs.opam
-          ];
-
-        shellHook = ''
-          # eval $(opam env)
-        '';
-      };
-  in {
-    devShell = inputs.nixpkgs.lib.genAttrs systems createDevShell;
-  };
+    {
+      devShell = inputs.nixpkgs.lib.genAttrs systems createDevShell;
+    };
 }
