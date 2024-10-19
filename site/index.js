d154be69bc46a480726f8876bad4b080ca500f80
Jayesh Bhoot <jayesh@bhoot.dev>
2024-10-19 12:01:07 +0530

    add copy link button besides headings with id

--- site/index.js
+++ site/index.js
@@ -16,13 +16,14 @@ const addLinkButtonToHeadings = () => {
             btn.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link "><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
             `;
-            
+
             btn.type = 'button';
+            btn.classList.add("copy-link");
             btn.onclick = async e => {
                 const linkToCopy = window.location.href + "#" + heading.id;
                 await navigator.clipboard.writeText(linkToCopy);
             };
-            heading.parentNode.insertBefore(btn, heading);
+            heading.appendChild(btn);
         }
     }
 };
