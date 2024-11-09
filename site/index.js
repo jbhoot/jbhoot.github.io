380ce39177ed04e47bc14dc3a769de8d5add98c6
Jayesh Bhoot <jayesh@bhoot.dev>
2024-11-09 22:58:17 +0530

    enhance copy-link button's function and styles

--- site/index.js
+++ site/index.js
@@ -9,19 +9,23 @@ const addListRoleToTimelines = () => {
 };
 
 const addLinkButtonToHeadings = () => {
+    const chainSvg = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link "><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
+            `;
     const headings = document.querySelectorAll(':is(h1, h2, h3, h4, h5, h6)');
     for (const heading of headings) {
         if (heading.id) {
             const btn = document.createElement('button');
-            btn.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link "><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
-            `;
-
+            btn.innerHTML = chainSvg;
             btn.type = 'button';
             btn.classList.add("copy-link");
             btn.onclick = async e => {
                 const linkToCopy = window.location.href + "#" + heading.id;
                 await navigator.clipboard.writeText(linkToCopy);
+                btn.innerText = '✅ Link copied!'
+                window.setTimeout(() => {
+                    btn.innerHTML = chainSvg;
+                }, 2000);
             };
             heading.appendChild(btn);
         }
