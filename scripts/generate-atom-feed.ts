2837d8aeba225190a83d8e49c4a43fe7a86f4838
Jayesh Bhoot <jayesh@bhoot.dev>
2025-03-02 13:21:07 +0530

    reduce deps by trying to use only effect-ts

--- scripts/generate-atom-feed.ts
+++ scripts/generate-atom-feed.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs/promises';
-import escape from 'lodash/escape.js';
-import { isAfter } from 'date-fns';
-import { Effect, Option, pipe, Schema } from "effect"
+import { Effect, Option, pipe, Schema } from "effect";
+import { greaterThan, unsafeMake } from "effect/DateTime";
 
 const EntrySchema = Schema.Struct({
   id: Schema.String,
@@ -23,25 +22,39 @@ const decoder = Schema.decodeUnknown(EntriesSchema);
 
 const baseUrl = "https://bhoot.dev";
 
+const escapeHtml = (s: string) => {
+  // Though the ">" character is escaped for symmetry, characters like ">" and "/" don't need escaping in HTML and have no special meaning unless they're part of a tag or unquoted attribute value. See Mathias Bynens's article (under "semi-related fun fact") for more details.
+  const EscapeMap: Record<string, string> = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#39;",
+  };
+  return s.replace(/[&<>"']/g, match => EscapeMap[match] || match);
+}
+
 const toXmlEntry = (e: Entry) => {
   const collections = e.collections
     .map(c => `<category term="${c}" label="${c}" />`)
     .join('\n');
 
-  const updated = e.updated.pipe(
+  const updated = pipe(
+    e.updated,
     Option.getOrElse(() => e.published),
-    v => `<updated>${v.toISOString()}</updated>`);
+    v => `<updated>${v.toISOString()}</updated>`
+  );
 
   return `<entry>
     <id>${e.id}</id>
-    <title type="html">${escape(e.title)}</title>
+    <title type="html">${escapeHtml(e.title)}</title>
     ${updated}
     <author>
       <name>${e.author_name}</name>
     </author>
-    <content type="html">${escape(e.content)}</content>
+    <content type="html">${escapeHtml(e.content)}</content>
     <link href="${e.url}"/>
-    <summary type="html">${escape(e.excerpt)}</summary>
+    <summary type="html">${escapeHtml(e.excerpt)}</summary>
     ${collections}
     <published>${e.published.toISOString()}</published>
   </entry>`;
@@ -52,15 +65,17 @@ const latestModified = (entries: readonly Entry[]) => {
     .reduce((acc: Option.Option<Date>, curr) => {
       const currUpdated = Option.getOrElse(curr.updated, () => curr.published);
       const accUpdated = Option.getOrElse(acc, () => currUpdated);
-      return isAfter(currUpdated, accUpdated)
+      return greaterThan(unsafeMake(currUpdated), unsafeMake(accUpdated))
         ? Option.some(currUpdated)
         : Option.some(accUpdated);
     }, Option.none());
 }
 
 const makeFeed = (entries: readonly Entry[]) => {
-  const latestModifiedTimeStamp = latestModified(entries)
-    .pipe(
+  const latestModifiedTimeStamp =
+    pipe(
+      entries,
+      latestModified,
       Option.map(v => `<updated>${v.toISOString()}</updated>`),
       Option.getOrElse(() => "")
     );
